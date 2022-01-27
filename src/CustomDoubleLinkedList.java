86788a7545ac755606754d17358c5373a2b1ce25
insu.choi <var@kakao.com>
2022-01-27 21:43:47 +0900

    [#5] Linked List 2
    prevNode와 nextNode를 가지는 double linked list

---
+++ src/CustomDoubleLinkedList.java
@@ -0,0 +1,82 @@
+public class CustomDoubleLinkedList<T> {
+
+    public class Node<T> {
+        T data;
+        Node<T> prev;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public Node<T> head = null;
+    public Node<T> tail = null;
+
+    public void add(T item) {
+        if (this.head == null) {
+            this.head = new Node<T>(item);
+            this.tail = this.head;
+        } else {
+            Node<T> node = this.head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new Node<T>(item);
+            node.next.prev = node;
+            this.tail = node.next;
+        }
+    }
+
+    public void printAll() {
+        if (this.head != null) {
+            Node<T> node = this.head;
+            System.out.println(node.data);
+            while (node.next != null) {
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public Node<T> searchFromHead(T item) {
+        if (this.head != null) {
+            Node<T> target = this.head;
+            while (target != null) {
+                if (target.data == item) {
+                    return target;
+                } else {
+                    target = target.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Node<T> searchFromTail(T item) {
+        if (this.head != null) {
+            Node<T> target = this.tail;
+            while (target != null) {
+                if (target.data == item) {
+                    return target;
+                } else {
+                    target = target.prev;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean addItemAfterData(T item, T data) {
+        Node<T> targetNode = this.searchFromHead(data);
+        if (targetNode != null) {
+            Node<T> backNode = targetNode.next;
+            targetNode.next = new Node<T>(item);
+            targetNode.next.next = backNode;
+            targetNode.next.prev = targetNode;
+            backNode.prev = targetNode.next;
+            return true;
+        }
+        return false;
+    }
+}
--- src/LinkedListLecture.java
+++ src/LinkedListLecture.java
@@ -11,5 +11,19 @@ public class LinkedListLecture {
         customLinkedList.printAll();
         customLinkedList.addItemAfterData(7, 4);
         customLinkedList.printAll();
+        System.out.println("----------------------------------------------");
+        CustomDoubleLinkedList<Integer> customDoubleLinkedList = new CustomDoubleLinkedList<Integer>();
+        customDoubleLinkedList.add(1);
+        customDoubleLinkedList.add(2);
+        customDoubleLinkedList.add(3);
+        customDoubleLinkedList.add(4);
+        customDoubleLinkedList.add(5);
+        customDoubleLinkedList.printAll();
+        System.out.println("----------------------------------------------");
+        customDoubleLinkedList.addItemAfterData(6, 2);
+        customDoubleLinkedList.printAll();
+        System.out.println("----------------------------------------------");
+        customDoubleLinkedList.addItemAfterData(7, 4);
+        customDoubleLinkedList.printAll();
     }
 }
